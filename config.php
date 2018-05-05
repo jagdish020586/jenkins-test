327007a7b88ab05b776600c88ffacabf0bf21d52
jagdish020586 <35000158+jagdish020586@users.noreply.github.com>
2018-05-05 15:44:36 +0530

    Add files via upload
    First

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kytest.kysecitsolutions.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kytest.kysecitsolutions.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/keysecit/public_html/kytest/catalog/');
+define('DIR_SYSTEM', '/home/keysecit/public_html/kytest/system/');
+define('DIR_LANGUAGE', '/home/keysecit/public_html/kytest/catalog/language/');
+define('DIR_TEMPLATE', '/home/keysecit/public_html/kytest/catalog/view/theme/');
+define('DIR_CONFIG', '/home/keysecit/public_html/kytest/system/config/');
+define('DIR_IMAGE', '/home/keysecit/public_html/kytest/image/');
+define('DIR_CACHE', '/home/keysecit/public_html/kytest/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/keysecit/public_html/kytest/system/storage/download/');
+define('DIR_LOGS', '/home/keysecit/public_html/kytest/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/keysecit/public_html/kytest/system/storage/modification/');
+define('DIR_UPLOAD', '/home/keysecit/public_html/kytest/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kytest');
+define('DB_PASSWORD', 'asdf1234');
+define('DB_DATABASE', 'kytest');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
